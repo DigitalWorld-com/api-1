3ab7362b2a3105247f696c5fddd7a8a9cd6bcdc7
martinmetallo <martinmetallo@gmail.com>
2023-06-01 23:21:40 -0300

    GetMapping que saluda!

--- src/main/java/com/example/grupo1/controllers/GreetingController.java
+++ src/main/java/com/example/grupo1/controllers/GreetingController.java
@@ -1,2 +1,17 @@
-package com.example.grupo1.controllers;public class GreetingController {
+package com.example.grupo1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController {
+
+    @GetMapping("/saludo")
+    public String saludar(){
+        return "Estoy saludando desde el grupo 1";
+    }
+
+
 }
