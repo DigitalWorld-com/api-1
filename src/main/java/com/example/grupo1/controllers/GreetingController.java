5f97370014ff6b65c6d929c6f0e3f5ed570bf5e3
martinmetallo <martinmetallo@gmail.com>
2023-06-04 16:36:04 -0300

    Guardar cambios en GreetingController.java antes de cambiar a la rama main

--- src/main/java/com/example/grupo1/controllers/GreetingController.java
+++ src/main/java/com/example/grupo1/controllers/GreetingController.java
@@ -1,11 +1,13 @@
 package com.example.grupo1.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GreetingController {
 
     @GetMapping("/greeting")
