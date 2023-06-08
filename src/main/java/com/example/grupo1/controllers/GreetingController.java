803c0fc24cda5e670b932325a754fae133667e1e
Caro Bima <carobima@gmail.com>
2023-06-08 05:00:09 -0300

    Modificando cors

--- src/main/java/com/example/grupo1/controllers/GreetingController.java
+++ src/main/java/com/example/grupo1/controllers/GreetingController.java
@@ -1,13 +1,12 @@
 package com.example.grupo1.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api1")
-@CrossOrigin(origins = "http://localhost:5173")
 public class GreetingController {
 
     @GetMapping("/saludos")
